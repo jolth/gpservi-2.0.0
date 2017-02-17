334d0bb1801936ad96ce11b5af5ab200e8031888
jolth <jolthgs@gmail.com>
2017-02-17 15:52:37 -0500

    add colum odometer

--- SQL/odometer.sql
+++ SQL/odometer.sql
@@ -1,10 +1,10 @@
 -- Add columns
 
 BEGIN;
-ALTER TABLE last_positions_gps ADD COLUMN odometer double precision;
+ALTER TABLE last_positions_gps ADD COLUMN odometer character varying(20);
 COMMIT;
 
 BEGIN;
-ALTER TABLE positions_gps ADD COLUMN odometer double precision;
+ALTER TABLE positions_gps ADD COLUMN odometer character varying(20);
 COMMIT;
 
