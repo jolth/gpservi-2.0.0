f1b3192edb82e8ccb05d6b354ce86c3b7815ed8b
jolth <jolthgs@gmail.com>
2017-05-24 15:59:02 -0500

    update mTokm

--- Gps/SkyPatrol/convert.py
+++ Gps/SkyPatrol/convert.py
@@ -85,4 +85,4 @@ def fechaHoraSkp(date, time):
     return dt.astimezone(timezone(str(utc)))
 
 def mTokm(meters):
-    return "{:,} km".format(int(meters) / 1000)
+    return "{:,}".format(int(meters) / 1000)
